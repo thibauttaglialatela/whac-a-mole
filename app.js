e5176361493b8d59f8f8eeac4c5505984dbb56bc
thibauttaglialatela <thibauttaglialatela@gmail.com>
2022-04-08 11:23:43 +0200

    whac-a-mole finished + last push

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const squares = document.querySelectorAll('.square');
 const mole = document.querySelector('.mole');
+let time = 60;
 const timeLeft = document.querySelector('#time-left');
 const score = document.querySelector('#score');
 
 let result = 0;
+let randomPosition;
+timeLeft.textContent = time;
+let timerId = null;
+
+
 
 function randomSquare() {
     squares.forEach(square => {
@@ -12,5 +18,37 @@ function randomSquare() {
 
     let randomSquare = squares[Math.floor(Math.random() * 9)]
     randomSquare.classList.add('mole');
+    randomPosition = randomSquare.id;
 }
-randomSquare();
+
+squares.forEach(square => {
+    square.addEventListener("mousedown", () => {
+        if (square.id === randomPosition) {
+            result++;
+            score.textContent = result;
+            randomPosition = null;
+        }
+    })
+
+})
+
+function moveMole() {
+    timerId = setInterval(randomSquare, 500);
+}
+
+moveMole();
+
+
+const decreaseTime = () => {
+    time = time <= 0 ? 0 : time -1;
+    timeLeft.textContent = time;
+    if (time === 0) {
+        clearInterval(timeLeftId);
+        clearInterval(timerId);
+        alert('GAME OVER! Your result is ' + result);
+    }
+}
+
+let timeLeftId = setInterval(decreaseTime, 1000);
+
+
